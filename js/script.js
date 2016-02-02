c4ef2ae8053ecbebe73e23043e2ea877448a46f5
Alin <alin.ratuoiu@cs.pub.ro>
2016-02-02 22:22:56 +0200

    Activate/deactivate navbar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
- $(document).ready(function(){
+ $(document).ready(function(){  
      
             $("body").scrollspy({
-                target: "#myScrollspy"
+                target: "#myScrollspy",
+                offset:50
             });
             
-            $("#myScrollspy").on("activate.bs.scrollspy", function(){
+            $("nav").css("visibility","hidden");
+            
+            $("#myScrollspy").on("activate.bs.scrollspy", function(target){
                 
-                console.log("event");
+                $("nav").css("visibility","visible");
+                if(target.target.children[0].hash === "#title"){
+                    $("nav").css("visibility","hidden");
+                }
                 
             });
+                   
+            $("#title").scroll(function(){
+                    $("nav").css("visibility","hidden"); 
+            })
             
             $('nav li').on("click", function(){
                $('.active').removeClass("active");
                $(this).addClass("active");
             });
+            
+            $("#svg1").attr("height", "0");
+            $("#svg1").attr("width", "0");
+            connectAll();
+                        
         });
+        
+$(document).on('scroll', '#title', function(){
+    $("nav").css("visibility","hidden"); 
+});
+
+$(window).resize(function () {
+    $("#svg1").attr("height", "0");
+    $("#svg1").attr("width", "0");
+    connectAll();
+});
