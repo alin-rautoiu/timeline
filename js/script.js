435243cd269f17dd7b8fa1c5fb4b878f7da3874d
alin.rautoiu <alin.rautoiu@devpadawan.ro>
2016-02-05 18:52:50 +0200

    Lines

--- js/script.js
+++ js/script.js
@@ -1,49 +1,29 @@
  $(document).ready(function(){  
      
-            $("body").scrollspy({
-                target: "#myScrollspy",
-                offset:50
-            });
-            
-            $("nav").css("visibility","hidden");
-            
-            $("#myScrollspy").on("activate.bs.scrollspy", function(target){
-                
-                $("nav").css("visibility","visible");
-                if(target.target.children[0].hash === "#title"){
-                    $("nav").css("visibility","hidden");
-                }
-                
-            });
-                   
-            $("#title").scroll(function(){
-                    $("nav").css("visibility","hidden"); 
-            })
-            
-            $('nav li').on("click", function(){
-               $('.active').removeClass("active");
-               $(this).addClass("active");
-            });
-            
-            $("#svg1").attr("height", "0");
-            $("#svg1").attr("width", "0");
-            connectAll();
-                        
-        });
+    $("body").scrollspy({
+        target: "#myScrollspy",
+        offset:50
+    });
+
+    $("nav").css("visibility","hidden");
+
+    $("#myScrollspy").on("activate.bs.scrollspy", function(target){
         
-$(document).on('scroll', '#title', function(){
-    $("nav").css("visibility","hidden"); 
-});
+        $("nav").css("visibility","visible");
+        
+        if(target.target.children[0].hash === "#title"){
+            $("nav").css("visibility","hidden");
+        }
+        
+    });
+            
+    $("#title").scroll(function(){
+            $("nav").css("visibility","hidden"); 
+    })
 
-$(window).resize(function () {
-    $("#svg1").attr("height", "0");
-    $("#svg1").attr("width", "0");
-    connectAll();
+    $('nav li').on("click", function(){
+        $('.active').removeClass("active");
+        $(this).addClass("active");
+    });
+       
 });
-
-function connectAll() {
-    // connect all the paths you want!
-    connectElements($("#svg1"), $("#myNewPath"), $("#section1"),  $("#firstpoint"));
-    connectElements($("#svg1"), $("#someOtherPath"), $("#section2"), $("#section3")  );
-    connectElements($("#svg1"), $("#yetAnotherPath"), $("#section3"), $("#section4")  );
-}
